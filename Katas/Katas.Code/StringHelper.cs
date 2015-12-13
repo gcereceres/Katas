4958903950cf4f7925cb17711d4fde6bbb6f69ef
elcanno@gmail.com <elcanno@gmail.com>
2015-12-13 12:39:07 -0700

    Se agrego el metodo ReverseString y el metodo WordCount.

---
+++ Katas/Katas.Code/StringHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Code
+{
+    public class StringHelper
+    {
+        public String ReverseString(string inputString)
+        {
+            return String.Concat(inputString.Reverse());
+        }
+
+        public Int32 WordCount(string inputString1)
+        {   
+            return inputString1.Split(' ').Count();
+
+        }
+    }
+}
